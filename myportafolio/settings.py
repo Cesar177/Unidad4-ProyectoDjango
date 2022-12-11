83977d2c4bd904fc2b4f4ce6b65a29f50ff7c8ea
cesar <cquesr175@gmail.com>
2022-12-11 13:38:15 -0500

    config settings cambio de config

--- myportafolio/settings.py
+++ myportafolio/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'myportafolio/static'),)
 # STATIC_ROOT =os.path.join(BASE_DIR, 'myportafolio/staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myportafolio/static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
